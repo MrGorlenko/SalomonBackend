169f8671f680f7ada29ac3785292d9fd87f37256
Uroky1 <ilu6a234@gmail.com>
2021-06-30 01:58:20 +0400

    add_to_cart_commit_fix

--- main/mixins.py
+++ main/mixins.py
@@ -7,13 +7,14 @@ class CartMixin(View):
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             customer = Customer.objects.filter(session_id=request.session.session_key).first()
+            last_cart_id = Cart.objects.latest('id')
             if not customer:
                 customer = Customer.objects.create(
                     session_id=request.session.session_key
                 )
             cart = Cart.objects.filter(owner=customer, in_order=False).first()
             if not cart:
-                cart = Cart.objects.create(owner=customer)
+                cart = Cart.objects.create(id=str(last_cart_id.id + 1), owner=customer)
         else:
             cart = Cart.objects.filter(for_anonymous_user=True).first()
             if not cart:

9b351d78f102af148b266576a4e0d8fd970db75e
Uroky1 <ilu6a234@gmail.com>
2021-07-01 14:07:21 +0400

    test_ip

--- main/mixins.py
+++ main/mixins.py
@@ -5,19 +5,30 @@ from .models import *
 class CartMixin(View):
 
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated:
-            customer = Customer.objects.filter(session_id=request.session.session_key).first()
-            last_cart_id = Cart.objects.latest('id')
-            if not customer:
-                customer = Customer.objects.create(
-                    session_id=request.session.session_key
-                )
-            cart = Cart.objects.filter(owner=customer, in_order=False).first()
-            if not cart:
-                cart = Cart.objects.create(id=str(last_cart_id.id + 1), owner=customer)
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
         else:
-            cart = Cart.objects.filter(for_anonymous_user=True).first()
+            ip = request.META.get('REMOTE_ADDR')
+        if Cart.objects.exclude(products__isnull=True):
+            last_cart_id = 0
+        else:
+            last_cart_id = Cart.objects.latest('id')
+
+        if not request.user.is_authenticated:
+            #customer = Customer.objects.filter(session_id=request.session.session_key).first()
+            #if not customer:
+            #    customer = Customer.objects.create(
+            #        session_id=request.session.session_key
+            #    )
+            #cart = Cart.objects.filter(owner=customer, in_order=False).first()
+            #if not cart:
+            #    cart = Cart.objects.create(id=str(last_cart_id.id + 1), owner=customer, in_order=True)
+        #else:
+            cart = Cart.objects.filter().first()
             if not cart:
-                cart = Cart.objects.create(for_anonymous_user=True)
+                cart = Cart.objects.create(id=str(last_cart_id.id + 1),
+                                           owner=str(ip), in_order=True)
+                print(cart)
         self.cart = cart
         return super().dispatch(request, *args, **kwargs)

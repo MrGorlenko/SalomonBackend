f9dbc4800f310d7524a5161d90e097fdb25f546a
Uroky1 <ilu6a234@gmail.com>
2021-07-13 15:29:34 +0400

    gmail_smtp

--- settings/settings.py
+++ settings/settings.py
@@ -8,12 +8,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'Uroky322@yandex.ru'
-EMAIL_HOST_PASSWORD = '#######'
-DEFAULT_FROM_EMAIL = 'ilu6a234@gmail.com'
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ilu6a234@gmail.com'
+EMAIL_HOST_PASSWORD = '#######'
+EMAIL_USE_SSL = False
 
 INSTALLED_APPS = [
     'jet.dashboard',
